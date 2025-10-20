8ff07133eb291335004eeb77f3ef6d4cb79eaba9
= <=>
2025-10-19 22:32:23 -0300

    ok

--- src/common/interceptors/logger.interceptors.ts
+++ src/common/interceptors/logger.interceptors.ts
@@ -3,7 +3,8 @@ import { Observable } from "rxjs";
 
 export class LoggerInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler<any>):Observable<any> | Promise<Observable<any>> {
-        console.log('Interceptando essa requisição');
+        
+        
         return next.handle().pipe();
     }
 }
