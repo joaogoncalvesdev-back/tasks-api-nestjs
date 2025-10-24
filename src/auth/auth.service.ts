b0050aabc3dd69ecb7bfe43ddeac18067af81e95
= <=>
2025-10-24 14:10:52 -0300

    nova att

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,23 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { SigninDto } from './dto/signin.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { HashingServiceProtocol } from './hash/hashing.service';
+import jwtConfig from './config/jwt.config';
+import type { ConfigType } from '@nestjs/config'; // ← Changed to import type
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly prisma: PrismaService,
     private readonly hashingService: HashingServiceProtocol,
-  ) {}
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+    private readonly jwtService: JwtService,
+  ) {
+    console.log(this.jwtConfiguration);
+  }
+  // ... rest of your code
 
   async authenticate(signinDto: SigninDto) {
     const user = await this.prisma.user.findFirst({
@@ -21,12 +30,27 @@ export class AuthService {
       throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
     }
 
-    const passwordIsValid = await this.hashingService.compare(signinDto.password, user.passwordHash);
+    const passwordIsValid = await this.hashingService.compare(
+      signinDto.password,
+      user.passwordHash,
+    );
     if (!passwordIsValid) {
       throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
     }
 
-    return{ message: 'Authentication successful' };
+    const token = await this.jwtService.signAsync(
+      {
+        sub: user.id,
+        email: user.email,
+      },
+      {
+        secret: this.jwtConfiguration.secret,
+        expiresIn: this.jwtConfiguration.jwtTtl,
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+      },
+    );
 
+    return { message: 'Authentication successful' };
   }
 }
